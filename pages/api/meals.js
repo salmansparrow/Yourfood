53fdb3425cbabbc99cfc2b2730cde30b896d79ee
salman <truesalman7@gmail.com>
2024-07-15 16:55:30 +0500

    slash done

--- pages/api/meals.js
+++ pages/api/meals.js
@@ -15,11 +15,22 @@ import { ConnectToDataBase } from "@/lib/mongodb";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
+    const { page = 1, pageSize = 10 } = req.query;
+
     try {
       const db = await ConnectToDataBase();
       const collection = db.collection("meals");
-      const meals = await collection.find().toArray();
-      res.status(200).json({ meals });
+
+      const totalCount = await collection.countDocuments();
+      const totalPages = Math.ceil(totalCount / pageSize);
+
+      const meals = await collection
+        .find()
+        .skip((page - 1) * pageSize)
+        .limit(pageSize)
+        .toArray();
+
+      res.status(200).json({ meals, totalPages });
     } catch (error) {
       console.error("Failed to fetch meals:", error);
       res.status(500).json({ error: "Failed to fetch meals" });
